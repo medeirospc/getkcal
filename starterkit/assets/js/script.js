d3677acfe47f2512f5043813b501378216da0c9f
Gustavo Medeiros <eugus03@gmail.com>
2022-12-20 18:13:29 -0300

    update script.js

--- starterkit/assets/js/script.js
+++ starterkit/assets/js/script.js
@@ -1,27 +1,26 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
+const form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const gender = getSelectedValue('gender');
-    const age = getInputNumberValue('age');
-    const weight = getInputNumberValue('weight');
-    const height = getInputNumberValue('height');
-    const activityLevel = getSelectedValue('activity_level');
+  const gender = getSelectedValue("gender");
+  const age = getInputNumberValue("age");
+  const weight = getInputNumberValue("weight");
+  const height = getInputNumberValue("height");
+  const activityLevel = getSelectedValue("activity_level");
 
-    /*taxa metabólica basal*/
-    const tmb = Math.round(
-        gender === 'female'
-            ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
-            : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
-    );
+  const tmb = Math.round(
+    gender === "female"
+      ? 655 + 9.6 * weight + 1.8 * height - 4.7 * age
+      : 66 + 13.7 * weight + 5 * height - 6.8 * age
+  );
 
-    const maintenance = Math.round(tmb * Number(activityLevel));
-    const loseWeight = maintenance - 450;
-    const gainWeight = maintenance + 450;
+  const maintenance = Math.round(tmb * Number(activityLevel));
+  const loseWeight = maintenance - 450;
+  const gainWeight = maintenance + 450;
 
-    const layout = `
+  const layout = `
         <h2>Resultado:</h2>
 
         <div class="result-content">
@@ -42,16 +41,15 @@ function handleSubmit(event) {
     </div> 
 `;
 
-    const result = document.getElementById('result');
-    result.innerHTML = layout;
+  const result = document.getElementById("result");
+  result.innerHTML = layout;
 }
 
 function getSelectedValue(id) {
-    const select = document.getElementById(id);
-    return select.options[select.selectedIndex].value;
-
+  const select = document.getElementById(id);
+  return select.options[select.selectedIndex].value;
 }
 
 function getInputNumberValue(id) {
-    return Number(document.getElementById(id).value);
+  return Number(document.getElementById(id).value);
 }
